6575660b6e53eb73d06b3418f925c87725ce0065
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-17 08:09:44 -0400

    Initial commit

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,12 @@
 "use strict";
 const npm_cache = 'npm-cache';
 const sw = self;
-sw.addEventListener('fetch', event => {
+sw.addEventListener('fetch', async (event) => {
+    if (event.request.url.indexOf('@') > -1) {
+        const response = await caches.match(event.request);
+        if (response)
+            return;
+    }
     event.respondWith(caches.open(npm_cache).then(function (cache) {
         return fetch(event.request).then(function (response) {
             cache.put(event.request, response.clone());
@@ -9,7 +14,7 @@ sw.addEventListener('fetch', event => {
         });
     }));
 });
-sw.addEventListener('fetch', event => {
+sw.addEventListener('fetch', async (event) => {
     event.respondWith(caches.match(event.request).then(function (response) {
         return response || fetch(event.request);
     }));
--- public/sw.ts
+++ public/sw.ts
@@ -2,23 +2,27 @@ const npm_cache = 'npm-cache';
 
 const sw = self as any as ServiceWorkerGlobalScope;
 
-sw.addEventListener('fetch', function(event: FetchEvent) {
-  event.respondWith(
-    caches.open(npm_cache).then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    })
-  );
+sw.addEventListener('fetch', async event => {
+    if (event.request.url.indexOf('@') > -1) {
+        const response = await caches.match(event.request);
+        if (response) return;
+    }
+    event.respondWith(
+        caches.open(npm_cache).then(function (cache) {
+            return fetch(event.request).then(function (response) {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        })
+    );
 });
 
 
 
-sw.addEventListener('fetch', function(event: FetchEvent) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+sw.addEventListener('fetch', async event => {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
