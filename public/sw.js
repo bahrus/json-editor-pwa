d6da454ad889c85edbe985841981f64af43f4645
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-17 08:55:53 -0400

    Initial commit

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,35 @@
 "use strict";
 const npm_cache = 'npm-cache';
 const sw = self;
-sw.addEventListener('fetch', async (event) => {
+sw.addEventListener('fetch', event => {
     if (event.request.url.indexOf('@') > -1) {
-        const response = await caches.match(event.request);
-        if (response)
-            return;
-    }
-    event.respondWith(caches.open(npm_cache).then(function (cache) {
-        return fetch(event.request).then(function (response) {
-            cache.put(event.request, response.clone());
-            return response;
+        caches.match(event.request).then(response => {
+            if (!response) {
+                console.log({ url: event.request.url, fn: 'fetch1' });
+                event.respondWith(caches.open(npm_cache).then(function (cache) {
+                    return fetch(event.request).then(function (response) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+                }));
+            }
         });
-    }));
+    }
+    else {
+        event.respondWith(caches.open(npm_cache).then(function (cache) {
+            return fetch(event.request).then(function (response) {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        }));
+    }
 });
 sw.addEventListener('fetch', async (event) => {
     event.respondWith(caches.match(event.request).then(function (response) {
+        if (response) {
+            return response;
+        }
+        console.log({ url: event.request.url, fn: 'fetch2' });
         return response || fetch(event.request);
     }));
 });
