8b0b8c737629ce83fcb2702b8b41b50c76756121
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-17 09:01:17 -0400

    Initial commit

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ sw.addEventListener('fetch', event => {
     if (event.request.url.indexOf('@') > -1) {
         caches.match(event.request).then(response => {
             if (!response) {
-                console.log({ url: event.request.url, fn: 'fetch1' });
+                //console.log({url: event.request.url, fn: 'fetch1'});
                 //event.respondWith(
                 caches.open(npm_cache).then(function (cache) {
                     return fetch(event.request).then(function (response) {
@@ -33,7 +33,7 @@ sw.addEventListener('fetch', async (event) => {
         if (response) {
             return response;
         }
-        console.log({ url: event.request.url, fn: 'fetch2' });
+        //console.log({url: event.request.url, fn: 'fetch2'});
         return response || fetch(event.request);
     }));
 });
--- public/sw.ts
+++ public/sw.ts
@@ -6,7 +6,7 @@ sw.addEventListener('fetch', event => {
     if (event.request.url.indexOf('@') > -1) {
         caches.match(event.request).then(response => {
             if(!response) {
-                console.log({url: event.request.url, fn: 'fetch1'});
+                //console.log({url: event.request.url, fn: 'fetch1'});
                 //event.respondWith(
                     caches.open(npm_cache).then(function (cache) {
                         return fetch(event.request).then(function (response) {
@@ -37,7 +37,7 @@ sw.addEventListener('fetch', async event => {
             if (response) {
                 return response;
             }
-            console.log({url: event.request.url, fn: 'fetch2'});
+            //console.log({url: event.request.url, fn: 'fetch2'});
             return response || fetch(event.request);
         })
     );
