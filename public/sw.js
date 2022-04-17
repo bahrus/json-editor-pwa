fa74e83d313ca10b1f282bf6572e8b63acd2f55c
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-17 08:00:22 -0400

    Initial commit

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 const npm_cache = 'npm-cache';
 const sw = self;
-sw.addEventListener('fetch', function (event) {
+sw.addEventListener('fetch', event => {
     event.respondWith(caches.open(npm_cache).then(function (cache) {
         return fetch(event.request).then(function (response) {
             cache.put(event.request, response.clone());
@@ -9,7 +9,7 @@ sw.addEventListener('fetch', function (event) {
         });
     }));
 });
-sw.addEventListener('fetch', function (event) {
+sw.addEventListener('fetch', event => {
     event.respondWith(caches.match(event.request).then(function (response) {
         return response || fetch(event.request);
     }));
